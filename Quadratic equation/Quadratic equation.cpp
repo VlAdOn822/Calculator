db37df4857eca15d6cf04c02726e6def01558890
VlAdOn822 <72297860+VlAdOn822@users.noreply.github.com>
2021-10-16 09:30:35 +0300

    Quadratic equation pro

--- Quadratic equation/Quadratic equation.cpp
+++ Quadratic equation/Quadratic equation.cpp
@@ -9,19 +9,17 @@ int main()
     int a, b, c;
     cout << "Enter a, b, c in quadratic equation\n";
     cin >> a >> b >> c;
-    int d;
-    d = b * b - 4 * a * c;
+
+    int d = b * b - 4 * a * c;
     if (d > 0)
     {
-        int x1, x2;
-        x1 = (-b + sqrt(d)) / 2 * a;
-        x2 = (-b - sqrt(d)) / 2 * a;
+        int x1 = (-b + sqrt(d)) / (2 * a);
+        int x2 = (-b - sqrt(d)) / (2 * a);
         cout << "x1 = " << x1 << "\n" << "x2 = " << x2;
     }
     else if (d == 0)
     {
-        int x;
-        x = -(b / 2 * a);
+        int x = -(b / (2 * a));
         cout << "x = " << x;
     }
     else
