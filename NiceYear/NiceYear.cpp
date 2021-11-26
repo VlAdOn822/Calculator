20643b520e7744f456d46cfee79b7490de3bf5ee
VlAdOn822 <vladk200430@gmail.com>
2021-11-26 21:18:49 +0200

    hw

---
+++ NiceYear/NiceYear.cpp
@@ -0,0 +1,31 @@
+// NiceYear.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int year, a, b, c, d;
+    cin >> year;
+    do
+    {
+        year++;
+        a = year / 1000;
+        b = year % 1000 / 100;
+        c = year % 100 / 10;
+        d = year % 10;
+    } while (a == b || a == c || a == d || b == c || b == d || c == d);
+    cout << year;
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
